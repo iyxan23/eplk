447a7ef48ecce8382d4043c7bbe4baa7e4dd604d
Iyxan23 <nurihsanalghifari@gmail.com>
2021-06-18 14:26:11 +0700

    feat: Create eplk objects

---
+++ eplk/src/main/java/com/iyxan23/eplk/objects/EplkInteger.kt
@@ -0,0 +1,6 @@
+package com.iyxan23.eplk.objects
+
+import com.iyxan23.eplk.lexer.models.Token
+
+class EplkInteger(override val token: Token) : EplkObject(token) {
+}
---
+++ eplk/src/main/java/com/iyxan23/eplk/objects/EplkObject.kt
@@ -0,0 +1,6 @@
+package com.iyxan23.eplk.objects
+
+import com.iyxan23.eplk.lexer.models.Token
+
+open class EplkObject(open val token: Token) {
+}
