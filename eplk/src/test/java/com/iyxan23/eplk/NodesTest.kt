ef024f96b0c123a32c45906a6dea76c144ebda73
Iyxan23 <nurihsanalghifari@gmail.com>
2021-06-19 06:26:44 +0700

    test: Add BinOp tests

--- eplk/src/test/java/com/iyxan23/eplk/NodesTest.kt
+++ eplk/src/test/java/com/iyxan23/eplk/NodesTest.kt
@@ -4,7 +4,9 @@ import com.iyxan23.eplk.interpreter.Scope
 import com.iyxan23.eplk.lexer.Lexer
 import com.iyxan23.eplk.lexer.models.Position
 import com.iyxan23.eplk.lexer.models.Token
+import com.iyxan23.eplk.nodes.BinOpNode
 import com.iyxan23.eplk.nodes.UnaryOpNode
+import com.iyxan23.eplk.objects.EplkFloat
 import com.iyxan23.eplk.objects.EplkInteger
 import com.iyxan23.eplk.parser.Parser
 import org.junit.Test
@@ -41,5 +43,52 @@ class NodesTest {
         assert(!resultVisit.hasError) { println(resultVisit.error) }
         assert(resultVisit.value is EplkInteger)
         assert((resultVisit.value as EplkInteger).value == 1)
+    }
+
+    @Test
+    fun testBinOpNode() {
+        val lexerResult = Lexer(filename, "1 + 1").doLexicalAnalysis()
+        val parseResult = Parser(lexerResult.tokens!!).parse().node as BinOpNode
+
+        println("Lexer result: $lexerResult")
+        println("Parse result: $parseResult")
+
+        val resultVisit = parseResult.visit(Scope(filename))
+
+        assert(!resultVisit.hasError) { println(resultVisit.error) }
+        assert(resultVisit.value is EplkInteger)
+        assert((resultVisit.value as EplkInteger).value == 2)
+    }
+
+    // FIXME: 6/19/21 This shouldn't return 2
+    @Test
+    fun testBinOpNode2() {
+        val lexerResult = Lexer(filename, "1 + 2 * 3 / 4").doLexicalAnalysis()
+        val parseResult = Parser(lexerResult.tokens!!).parse().node as BinOpNode
+
+        println("Lexer result: $lexerResult")
+        println("Parse result: $parseResult")
+
+        val resultVisit = parseResult.visit(Scope(filename))
+
+        assert(!resultVisit.hasError) { println(resultVisit.error) }
+        assert(resultVisit.value is EplkFloat)
+        println((resultVisit.value as EplkFloat).value)
+        assert((resultVisit.value as EplkFloat).value == 2.5f)
+    }
+
+    @Test
+    fun testBinOpNodeParentheses() {
+        val lexerResult = Lexer(filename, "(1 + 2) * 3").doLexicalAnalysis()
+        val parseResult = Parser(lexerResult.tokens!!).parse().node as BinOpNode
+
+        println("Lexer result: $lexerResult")
+        println("Parse result: $parseResult")
+
+        val resultVisit = parseResult.visit(Scope(filename))
+
+        assert(!resultVisit.hasError) { println(resultVisit.error) }
+        assert(resultVisit.value is EplkInteger)
+        assert((resultVisit.value as EplkInteger).value == 9)
     }
 }
