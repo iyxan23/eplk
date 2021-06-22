7c4268b63519ade4b74e93ae9b1b03c1ba0742e3
Iyxan23 <nurihsanalghifari@gmail.com>
2021-06-22 10:39:15 +0700

    ParserTests: Add test for 'for' statement

--- eplk/src/test/java/com/iyxan23/eplk/ParserTest.kt
+++ eplk/src/test/java/com/iyxan23/eplk/ParserTest.kt
@@ -130,6 +130,17 @@ class ParserTest {
     }
 
     @Test
+    fun forTest() {
+        val code = "for (var b = 0; b < 10; var b = b + 1) 1 + 1"
+        val tokens = Lexer(filename, code).doLexicalAnalysis().tokens!!
+        val result = Parser(tokens).parse()
+
+        assert(!result.hasError) { println(result.error!!.toString(code)) }
+
+        println(Utils.prettyPrintNode(result.node!!))
+    }
+
+    @Test
     fun syntaxErrorTest() {
         val code = "1 2 3 /"
         val tokens = Lexer(filename, code).doLexicalAnalysis().tokens!!
