83abe4dced0cb0dfee4563d82125aac5988b535c
Iyxan23 <nurihsanalghifari@gmail.com>
2021-06-23 08:37:18 +0700

    Shell: feat: Add individual times

--- eplk/src/main/java/com/iyxan23/eplk/Shell.kt
+++ eplk/src/main/java/com/iyxan23/eplk/Shell.kt
@@ -5,7 +5,11 @@ import com.iyxan23.eplk.lexer.Lexer
 import com.iyxan23.eplk.objects.EplkVoid
 import com.iyxan23.eplk.parser.Parser
 
+// Shows on how much time does it took to execute a command
 const val withTime = true
+
+// This one is for showing individual times for the lexer, parser, and interpreter
+const val individualTimes = true
 
 fun main() {
     println("Welcome to the EPLK Shell!")
@@ -20,19 +24,29 @@ fun main() {
 
         val lexerResult = Lexer("<SHELL>", code).doLexicalAnalysis()
 
+        if (individualTimes) println("Lexer took ${System.currentTimeMillis() - startTime}ms")
+
         if (lexerResult.error != null) {
             println(lexerResult.error.toString(code))
             continue
         }
 
+        val parserStartTime = System.currentTimeMillis()
+
         val parseResult = Parser(lexerResult.tokens!!).parse()
+
+        if (individualTimes) println("Parser took ${System.currentTimeMillis() - parserStartTime}ms")
 
         if (parseResult.hasError) {
             println(parseResult.error!!.toString(code))
             continue
         }
 
+        val interpreterStartTime = System.currentTimeMillis()
+
         val interpreterResult = parseResult.node!!.visit(scope)
+
+        if (individualTimes) println("Interpreter took ${System.currentTimeMillis() - interpreterStartTime}ms")
 
         if (interpreterResult.hasError) {
             println(interpreterResult.error!!.toString(code))
