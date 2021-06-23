b2e8abd6cb71279070ada1f4b97720cd4ef94e7a
Iyxan23 <nurihsanalghifari@gmail.com>
2021-06-23 15:23:30 +0700

    Interpreter: fix: visitNode should return RealtimeResult<EplkObject>

--- eplk/src/main/java/com/iyxan23/eplk/interpreter/Interpreter.kt
+++ eplk/src/main/java/com/iyxan23/eplk/interpreter/Interpreter.kt
@@ -1,12 +1,13 @@
 package com.iyxan23.eplk.interpreter
 
 import com.iyxan23.eplk.nodes.Node
+import com.iyxan23.eplk.objects.EplkObject
 
 /**
  * A very simple singleton object used for decoration purposes
  */
 object Interpreter {
-    fun visitNode(node: Node, scope: Scope) {
-        node.visit(scope)
+    fun visitNode(node: Node, scope: Scope): RealtimeResult<EplkObject> {
+        return node.visit(scope)
     }
 }
