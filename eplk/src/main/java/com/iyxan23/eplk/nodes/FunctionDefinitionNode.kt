727e9103a2a4f1ca67f9ae44ba79242c81c17781
Iyxan23 <nurihsanalghifari@gmail.com>
2021-06-23 15:15:28 +0700

    Nodes: fix: FuncDefNode should add the function to the scope instead of returning it

--- eplk/src/main/java/com/iyxan23/eplk/nodes/FunctionDefinitionNode.kt
+++ eplk/src/main/java/com/iyxan23/eplk/nodes/FunctionDefinitionNode.kt
@@ -5,6 +5,7 @@ import com.iyxan23.eplk.interpreter.Scope
 import com.iyxan23.eplk.lexer.models.Position
 import com.iyxan23.eplk.objects.EplkFunction
 import com.iyxan23.eplk.objects.EplkObject
+import com.iyxan23.eplk.objects.EplkVoid
 
 class FunctionDefinitionNode(
     val functionName: String,
@@ -15,8 +16,9 @@ class FunctionDefinitionNode(
     override val endPosition: Position = expression.endPosition
 
     override fun visit(scope: Scope): RealtimeResult<EplkObject> {
-        return RealtimeResult<EplkObject>().success(EplkFunction(
-            scope, functionName, parameters, expression
-        ))
+        scope.symbolTable.variables[functionName] =
+            EplkFunction(scope, functionName, parameters, expression)
+
+        return RealtimeResult<EplkObject>().success(EplkVoid(scope))
     }
 }
