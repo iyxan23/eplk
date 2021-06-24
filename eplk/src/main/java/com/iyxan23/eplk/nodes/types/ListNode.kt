ecbfd5be8291bdaf7b66b7d2b1a35b7887fd70ee
Iyxan23 <nurihsanalghifari@gmail.com>
2021-06-25 06:49:35 +0700

    Nodes: feat: add ListNode

---
+++ eplk/src/main/java/com/iyxan23/eplk/nodes/types/ListNode.kt
@@ -0,0 +1,31 @@
+package com.iyxan23.eplk.nodes.types
+
+import com.iyxan23.eplk.interpreter.RealtimeResult
+import com.iyxan23.eplk.interpreter.Scope
+import com.iyxan23.eplk.lexer.models.Position
+import com.iyxan23.eplk.nodes.Node
+import com.iyxan23.eplk.objects.EplkList
+import com.iyxan23.eplk.objects.EplkObject
+
+class ListNode(
+    override val startPosition: Position,
+    override val endPosition: Position,
+
+    val items: ArrayList<Node> = ArrayList(),
+) : Node() {
+
+    override fun visit(scope: Scope): RealtimeResult<EplkObject> {
+        // Evaluate each items
+        val result = RealtimeResult<EplkObject>()
+        val resultItems = ArrayList<EplkObject>()
+
+        items.forEach { node ->
+            val nodeResult = result.register(node.visit(scope))
+            if (result.hasError) return result
+
+            resultItems.add(nodeResult!!)
+        }
+
+        return result.success(EplkList(scope, resultItems))
+    }
+}
