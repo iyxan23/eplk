297d14f3ad0b2bc405225e3042e47ca8687e8478
Iyxan23 <nurihsanalghifari@gmail.com>
2021-06-25 06:50:35 +0700

    ParserTests: Add list tests

--- eplk/src/test/java/com/iyxan23/eplk/ParserTest.kt
+++ eplk/src/test/java/com/iyxan23/eplk/ParserTest.kt
@@ -196,6 +196,28 @@ class ParserTest {
     }
 
     @Test
+    fun listTest() {
+        val code = "[1, \"Hi\", -1, 1 + 1]"
+        val tokens = Lexer(filename, code).doLexicalAnalysis().tokens!!
+        val result = Parser(tokens).parse()
+
+        assert(!result.hasError) { println(result.error!!.toString()) }
+
+        println(Utils.prettyPrintNode(result.node!!))
+    }
+
+    @Test
+    fun emptyListTest() {
+        val code = "[]"
+        val tokens = Lexer(filename, code).doLexicalAnalysis().tokens!!
+        val result = Parser(tokens).parse()
+
+        assert(!result.hasError) { println(result.error!!.toString()) }
+
+        println(Utils.prettyPrintNode(result.node!!))
+    }
+
+    @Test
     fun syntaxErrorTest() {
         val code = "1 2 3 /"
         val tokens = Lexer(filename, code).doLexicalAnalysis().tokens!!
