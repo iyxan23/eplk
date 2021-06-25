da8658be026609f229a319a152fdc3738e8212f6
Iyxan23 <nurihsanalghifari@gmail.com>
2021-06-26 06:21:51 +0700

    Objects: feat: Add EplkNativeFunction for writing functions in java/kotlin

---
+++ eplk/src/main/java/com/iyxan23/eplk/objects/EplkNativeFunction.kt
@@ -0,0 +1,39 @@
+package com.iyxan23.eplk.objects
+
+import com.iyxan23.eplk.interpreter.RealtimeResult
+import com.iyxan23.eplk.interpreter.Scope
+import com.iyxan23.eplk.lexer.models.Position
+
+// Note: What I meant by native is that this function is made in the java/kotlin language
+class EplkNativeFunction(
+    scope: Scope,
+    val functionName: String,
+    val parameters: Array<String>,
+    val functionCallback:
+        (
+            scope: Scope,
+            arguments: Array<EplkObject>,
+            startPosition: Position,
+            endPosition: Position
+        ) -> RealtimeResult<EplkObject>
+
+) : EplkObject(scope) {
+    override val objectName: String = "Native function"
+    override fun toString(): String = "Native function $functionName(${parameters.joinToString(", ")})"
+
+    interface EplkNativeFunctionCallback {
+        fun call(
+            scope: Scope,
+            arguments: Array<EplkObject>,
+            startPosition: Position,
+            endPosition: Position
+        ): RealtimeResult<EplkObject>
+    }
+
+    override fun call(
+        arguments: Array<EplkObject>,
+        startPosition: Position,
+        endPosition: Position
+    ): RealtimeResult<EplkObject>
+        = functionCallback.invoke(scope, arguments, startPosition, endPosition)
+}
