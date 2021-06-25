70d70cc5fb39ecba4e6918a9fd459cd0258c573e
Iyxan23 <nurihsanalghifari@gmail.com>
2021-06-26 06:22:06 +0700

    Interpreter: feat: Add a minimal standard global definitions

---
+++ eplk/src/main/java/com/iyxan23/eplk/interpreter/StandardDefinitions.kt
@@ -0,0 +1,34 @@
+package com.iyxan23.eplk.interpreter
+
+import com.iyxan23.eplk.errors.runtime.EplkTypeError
+import com.iyxan23.eplk.lexer.models.Position
+import com.iyxan23.eplk.objects.EplkNativeFunction
+import com.iyxan23.eplk.objects.EplkObject
+import com.iyxan23.eplk.objects.EplkString
+import com.iyxan23.eplk.objects.EplkVoid
+
+object StandardDefinitions {
+    fun generateScope(name: String): Scope {
+        val scope = Scope(name)
+
+        scope.symbolTable.variables["println"] = EplkNativeFunction(scope, "println", arrayOf("text"))
+
+        { functionScope: Scope, arguments: Array<EplkObject>, startPosition: Position, endPosition: Position ->
+            val result = RealtimeResult<EplkObject>()
+            val text = arguments[0]
+
+            if (text !is EplkString) {
+                return@EplkNativeFunction  result.failure(EplkTypeError(
+                    "First argument must be a string, got ${text.objectName} instead",
+                    startPosition, endPosition, functionScope
+                ))
+            }
+
+            println(text.value)
+
+            return@EplkNativeFunction result.success(EplkVoid(functionScope))
+        }
+
+        return scope
+    }
+}
