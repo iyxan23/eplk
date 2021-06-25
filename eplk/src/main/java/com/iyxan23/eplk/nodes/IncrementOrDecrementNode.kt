699417c003a0c541936e2f34f192f699016e1225
Iyxan23 <nurihsanalghifari@gmail.com>
2021-06-26 06:54:55 +0700

    Nodes: feat: Add IncrementOrDecrementNode

---
+++ eplk/src/main/java/com/iyxan23/eplk/nodes/IncrementOrDecrementNode.kt
@@ -0,0 +1,45 @@
+package com.iyxan23.eplk.nodes
+
+import com.iyxan23.eplk.Tokens
+import com.iyxan23.eplk.interpreter.RealtimeResult
+import com.iyxan23.eplk.interpreter.Scope
+import com.iyxan23.eplk.lexer.models.Position
+import com.iyxan23.eplk.lexer.models.Token
+import com.iyxan23.eplk.objects.EplkObject
+
+class IncrementOrDecrementNode(
+    val incDecToken: Token,
+    val nodeToIncDec: Node
+
+) : Node() {
+    override val startPosition: Position
+        get() = nodeToIncDec.startPosition
+
+    override val endPosition: Position
+        get() = incDecToken.endPosition
+
+    override fun visit(scope: Scope): RealtimeResult<EplkObject> {
+        val result = RealtimeResult<EplkObject>()
+
+        val nodeToIncDecResult = result.register(nodeToIncDec.visit(scope))
+        if (result.hasError) return result
+
+        when (incDecToken.token) {
+            Tokens.DOUBLE_PLUS -> {
+                result.register(nodeToIncDecResult!!.increment(startPosition, endPosition))
+                if (result.hasError) return result
+
+                return result.success(nodeToIncDecResult)
+            }
+
+            Tokens.DOUBLE_MINUS -> {
+                result.register(nodeToIncDecResult!!.decrement(startPosition, endPosition))
+                if (result.hasError) return result
+
+                return result.success(nodeToIncDecResult)
+            }
+
+            else -> throw NotImplementedError("Token ${incDecToken.token.name} is not implemented in IncrementOrDecrementNode")
+        }
+    }
+}
