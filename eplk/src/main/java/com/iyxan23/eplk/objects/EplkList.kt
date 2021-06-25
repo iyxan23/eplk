c58c5e6e433fbcbc32623367179ece64802e0083
Iyxan23 <nurihsanalghifari@gmail.com>
2021-06-25 07:55:41 +0700

    Objects: feat: Implement plus operator for EplkList

--- eplk/src/main/java/com/iyxan23/eplk/objects/EplkList.kt
+++ eplk/src/main/java/com/iyxan23/eplk/objects/EplkList.kt
@@ -1,6 +1,8 @@
 package com.iyxan23.eplk.objects
 
+import com.iyxan23.eplk.interpreter.RealtimeResult
 import com.iyxan23.eplk.interpreter.Scope
+import com.iyxan23.eplk.lexer.models.Position
 
 class EplkList(
     scope: Scope,
@@ -10,4 +12,23 @@ class EplkList(
 
     override val objectName: String = "List"
     override fun toString(): String = "[${list.joinToString(", ")}]"
+
+    override fun operatorPlus(
+        other: EplkObject,
+        startPosition: Position,
+        endPosition: Position
+    ): RealtimeResult<EplkObject> {
+        val result = RealtimeResult<EplkObject>()
+
+        // Check if the other object is a list
+        if (other is EplkList) {
+            // Do addAll
+            return result.success(EplkList(scope, (list + other.list) as ArrayList<EplkObject>))
+        }
+
+        // If not, then add the object to the list
+        list.add(other)
+
+        return result.success(this)
+    }
 }
