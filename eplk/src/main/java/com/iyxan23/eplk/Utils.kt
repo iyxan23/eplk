b40e1f2d422bba7051e1ea302f982e95f0d4c011
Iyxan23 <nurihsanalghifari@gmail.com>
2021-06-27 05:56:28 +0700

    Utils: chore: Make prettyPrintNode's print nodes to be consistent

--- eplk/src/main/java/com/iyxan23/eplk/Utils.kt
+++ eplk/src/main/java/com/iyxan23/eplk/Utils.kt
@@ -18,60 +18,49 @@ object Utils {
 
         when (node) {
             is IntegerNode -> {
-                result.append(strIndentation + "Integer: ")
-                result.append(node.number.value)
+                result.append(strIndentation + "Integer: ${node.number.value}")
             }
 
             is FloatNode -> {
-                result.append(strIndentation + "Float: ")
-                result.append(node.float.value)
+                result.append(strIndentation + "Float: ${node.float.value}")
             }
 
             is BooleanNode -> {
-                result.append(strIndentation + "Boolean: ")
-                result.append(node.value)
+                result.append(strIndentation + "Boolean: ${node.value}")
             }
 
             is StringNode -> {
-                result.append(strIndentation + "String: ")
-                result.append(node.value)
+                result.append(strIndentation + "String: ${node.value}")
             }
 
             is ListNode -> {
                 result.appendLine(strIndentation + "List: ")
+
                 node.items.forEach { item ->
                     result.appendLine(prettyPrintNode(item, indentation + indentationAmount, indentationAmount))
                 }
             }
 
             is UnaryOpNode -> {
-                result.append(strIndentation + "Unary operation: operator: ")
-                result.append(node.tokenOperator.token.name)
-                result.appendLine()
-                result.append(prettyPrintNode(node.node, indentation + indentationAmount, indentationAmount))
+                result.appendLine(strIndentation + "Unary operation: operator: ${node.tokenOperator.token.name}")
+                result.appendLine(prettyPrintNode(node.node, indentation + indentationAmount, indentationAmount))
             }
 
             is BinOpNode -> {
-                result.append(strIndentation + "Binary operation: operator: ")
-                result.append(node.operatorToken.token.name)
-                result.appendLine()
-                result.append(prettyPrintNode(node.leftNode, indentation + indentationAmount, indentationAmount))
-                result.appendLine()
-                result.append(prettyPrintNode(node.rightNode, indentation + indentationAmount, indentationAmount))
+                result.appendLine(strIndentation + "Binary operation: operator: ${node.operatorToken.token.name}")
+                result.appendLine(prettyPrintNode(node.leftNode, indentation + indentationAmount, indentationAmount))
+                result.appendLine(prettyPrintNode(node.rightNode, indentation + indentationAmount, indentationAmount))
             }
 
             is VarDeclarationNode -> {
-                result.append(strIndentation + "Variable declaration:")
-                result.appendLine()
-                result.append(strIndentation + "Variable name: ${node.variableName}")
-                result.appendLine()
-                result.append(strIndentation + "Variable value: ")
-                result.appendLine()
-                result.append(prettyPrintNode(node.variableValue, indentation + indentationAmount, indentationAmount))
+                result.appendLine(strIndentation + "Variable declaration:")
+                result.appendLine(strIndentation + "Variable name: ${node.variableName}")
+                result.appendLine(strIndentation + "Variable value:")
+                result.appendLine(prettyPrintNode(node.variableValue, indentation + indentationAmount, indentationAmount))
             }
 
             is VarAccessNode -> {
-                result.append(strIndentation + "Variable access: Variable name: ${node.variableName}")
+                result.appendLine(strIndentation + "Variable access: Variable name: ${node.variableName}")
             }
 
             is IfNode -> {
@@ -79,51 +68,49 @@ object Utils {
                     val condition = pair.first
                     val expression = pair.second
 
-                    result.append(strIndentation + (if (index == 0) "If condition: \n" else "Elif condition: \n"))
-                    result.append(prettyPrintNode(condition, indentation + indentationAmount, indentationAmount))
-                    result.appendLine()
-                    result.append(strIndentation + "Expression: \n")
-                    result.append(prettyPrintNode(expression, indentation + indentationAmount, indentationAmount))
-                    result.appendLine()
+                    result.appendLine(strIndentation + (if (index == 0) "If condition:" else "Elif condition:"))
+                    result.appendLine(prettyPrintNode(condition, indentation + indentationAmount, indentationAmount))
+                    result.appendLine(strIndentation + "Expression:")
+                    result.appendLine(prettyPrintNode(expression, indentation + indentationAmount, indentationAmount))
                 }
 
-                result.append(strIndentation + "Else expression: \n")
-                result.append(prettyPrintNode(node.elseExpression, indentation + indentationAmount, indentationAmount))
+                result.appendLine(strIndentation + "Else expression: ")
+                result.appendLine(prettyPrintNode(node.elseExpression, indentation + indentationAmount, indentationAmount))
             }
 
             is ForNode -> {
-                result.append(strIndentation + "For statement:\n")
-                result.append(strIndentation + "First expression: \n")
+                result.appendLine(strIndentation + "For statement:")
+                result.appendLine(strIndentation + "First expression:")
                 result.appendLine(prettyPrintNode(node.firstExpression, indentation + indentationAmount, indentationAmount))
-                result.append(strIndentation + "Second expression: \n")
+                result.appendLine(strIndentation + "Second expression:")
                 result.appendLine(prettyPrintNode(node.secondExpression, indentation + indentationAmount, indentationAmount))
-                result.append(strIndentation + "Third expression: \n")
+                result.appendLine(strIndentation + "Third expression:")
                 result.appendLine(prettyPrintNode(node.thirdExpression, indentation + indentationAmount, indentationAmount))
-                result.append(strIndentation + "Expression: \n")
-                result.append(prettyPrintNode(node.expression, indentation + indentationAmount, indentationAmount))
+                result.appendLine(strIndentation + "Expression:")
+                result.appendLine(prettyPrintNode(node.expression, indentation + indentationAmount, indentationAmount))
             }
 
             is WhileNode -> {
-                result.append(strIndentation + "While statement: \n")
-                result.append(strIndentation + "Condition: \n")
+                result.appendLine(strIndentation + "While statement:")
+                result.appendLine(strIndentation + "Condition:")
                 result.appendLine(prettyPrintNode(node.condition, indentation + indentationAmount, indentationAmount))
-                result.append(strIndentation + "Expression: \n")
-                result.append(prettyPrintNode(node.expression, indentation + indentationAmount, indentationAmount))
+                result.appendLine(strIndentation + "Expression:")
+                result.appendLine(prettyPrintNode(node.expression, indentation + indentationAmount, indentationAmount))
             }
 
             is FunctionDefinitionNode -> {
-                result.append(strIndentation + "Function definition: \n")
-                result.append(strIndentation + "Name: ${node.functionName}\n")
-                result.append(strIndentation + "Parameters: ${node.parameters.joinToString(", ")}\n")
-                result.append(strIndentation + "Expression: \n")
+                result.appendLine(strIndentation + "Function definition:")
+                result.appendLine(strIndentation + "Name: ${node.functionName}")
+                result.appendLine(strIndentation + "Parameters: ${node.parameters.joinToString(", ")}")
+                result.appendLine(strIndentation + "Expression:")
                 result.appendLine(prettyPrintNode(node.expression, indentation + indentationAmount, indentationAmount))
             }
 
             is FunctionCallNode -> {
-                result.append(strIndentation + "Call function: \n")
-                result.append(strIndentation + "Node to call: \n")
+                result.appendLine(strIndentation + "Call function:")
+                result.appendLine(strIndentation + "Node to call:")
                 result.appendLine(prettyPrintNode(node.nodeToCall, indentation + indentationAmount, indentationAmount))
-                result.append(strIndentation + "Arguments: \n")
+                result.appendLine(strIndentation + "Arguments:")
 
                 node.arguments.forEach { argument ->
                     result.appendLine(prettyPrintNode(argument, indentation + indentationAmount, indentationAmount))
