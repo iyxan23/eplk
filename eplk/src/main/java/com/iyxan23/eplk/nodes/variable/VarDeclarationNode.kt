d2f853d71a4479169b5d23324005bd680754ca2a
Iyxan23 <nurihsanalghifari@gmail.com>
2021-06-27 09:33:18 +0700

    Nodes: tweak: Return an error when the same variable is declared twice

--- eplk/src/main/java/com/iyxan23/eplk/nodes/variable/VarDeclarationNode.kt
+++ eplk/src/main/java/com/iyxan23/eplk/nodes/variable/VarDeclarationNode.kt
@@ -1,5 +1,6 @@
 package com.iyxan23.eplk.nodes.variable
 
+import com.iyxan23.eplk.errors.runtime.EplkDefinitionError
 import com.iyxan23.eplk.interpreter.RealtimeResult
 import com.iyxan23.eplk.interpreter.Scope
 import com.iyxan23.eplk.lexer.models.Position
@@ -17,6 +18,16 @@ class VarDeclarationNode(
 
     override fun visit(scope: Scope): RealtimeResult<EplkObject> {
         val result = RealtimeResult<EplkObject>()
+
+        if (scope.symbolTable.variables.containsKey(variableName)) {
+            return result.failure(
+                EplkDefinitionError(
+                    "Variable $variableName is already declared in this scope",
+                    startPosition, endPosition, scope
+                )
+            )
+        }
+
         val visitValue = result.register(variableValue.visit(scope))
 
         if (result.hasError) return result
