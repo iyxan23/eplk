ba5dc9d2d19c5eaa6dc7788d4fcfb2b776b13286
Iyxan23 <nurihsanalghifari@gmail.com>
2021-06-30 13:46:19 +0700

    Nodes: feat: Add nodes for return, continue, and break

---
+++ eplk/src/main/java/com/iyxan23/eplk/nodes/control/BreakNode.kt
@@ -0,0 +1,17 @@
+package com.iyxan23.eplk.nodes.control
+
+import com.iyxan23.eplk.interpreter.RealtimeResult
+import com.iyxan23.eplk.interpreter.Scope
+import com.iyxan23.eplk.lexer.models.Position
+import com.iyxan23.eplk.nodes.Node
+import com.iyxan23.eplk.objects.EplkObject
+
+class BreakNode(
+    override val startPosition: Position,
+    override val endPosition: Position
+) : Node() {
+
+    override fun visit(scope: Scope): RealtimeResult<EplkObject> {
+        TODO("Not yet implemented")
+    }
+}
---
+++ eplk/src/main/java/com/iyxan23/eplk/nodes/control/ReturnNode.kt
@@ -0,0 +1,19 @@
+package com.iyxan23.eplk.nodes.control
+
+import com.iyxan23.eplk.interpreter.RealtimeResult
+import com.iyxan23.eplk.interpreter.Scope
+import com.iyxan23.eplk.lexer.models.Position
+import com.iyxan23.eplk.nodes.Node
+import com.iyxan23.eplk.objects.EplkObject
+
+class ReturnNode(
+    val returnValue: Node?,
+
+    override val startPosition: Position,
+    override val endPosition: Position
+) : Node() {
+
+    override fun visit(scope: Scope): RealtimeResult<EplkObject> {
+        TODO("Not yet implemented")
+    }
+}
